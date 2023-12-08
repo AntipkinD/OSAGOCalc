1b3e9130a63d496920b90205c118cafc40134c02
mushroomvoodoo <normeeboy@gmail.com>
2023-12-08 21:33:20 +0300

    Готов

--- OSAGOCalc/Form1.cs
+++ OSAGOCalc/Form1.cs
@@ -17,30 +17,35 @@ namespace OSAGOCalc
         {
             InitializeComponent();
         }
-        double rslt;
-        double bt;
-        double kt;
-        double kbm;
-        double ko;
-        double kvs;
-        double km;
-        double ks;
-        string tosave;
+        public double rslt;
+        public double bt;
+        public double kt;
+        public double kbm;
+        public double ko;
+        public double kvs;
+        public double km;
+        public double ks;
+        public string tosave;
         //Метод для расчета ОСАГО
+        void GetResult(double bt, double kt, double kbm, double ko, double kvs, double km, double ks, TextBox xtx)
+        {
+            double rslt = bt * kt * kbm * ko * kvs * km * ks;
+            xtx.Text = Convert.ToString(rslt);
+        }
+        //Кнопка для расчета ОСАГО
         public void Figureout_button_Click(object sender, EventArgs e)
         {
             if (double.TryParse(BT.Text, out this.bt) == true & double.TryParse(KT.Text, out this.kt) == true & double.TryParse(KBM.Text, out this.kbm) == true & double.TryParse(KO.Text, out this.ko) == true
                 & double.TryParse(KVS.Text, out this.kvs) == true & double.TryParse(KM.Text, out this.km) == true & double.TryParse(KS.Text, out this.ks) == true)
             {
-                this.rslt = this.bt * this.kt * this.kbm * this.ko * this.kvs * this.km * this.ks;
-                result.Text = Convert.ToString(this.rslt);
+                GetResult(this.bt, this.kt, this.kbm, this.ko, this.kvs, this.km, this.ks, result);
             }
-            else MessageBox.Show("Пожалуйста, введите или выберите числовые значения!", "Неверный ввод", MessageBoxButtons.OK);
+            else MessageBox.Show("Пожалуйста, введите или выберите числовые значения коэффициентов!", "Неверный ввод", MessageBoxButtons.OK);
         }
         //Метод для очистки полей
         public void Clear_button_Click(object sender, EventArgs e)
         {
-            BT.Text = "Базовый тариф(БТ)";
+            BT.Text = "Базовый тариф (БТ)";
             KT.Text = "Территориальный коэффициент (КТ)";
             KBM.Text = "Коэффициент \"бонус-малус\" (КБМ)";
             KO.Text = "Коэффициент ограничения (КО)";
@@ -53,11 +58,10 @@ namespace OSAGOCalc
         public void Export_button_Click(object sender, EventArgs e)
         {
             if (double.TryParse(BT.Text, out this.bt) == true & double.TryParse(KT.Text, out this.kt) == true & double.TryParse(KBM.Text, out this.kbm) == true & double.TryParse(KO.Text, out this.ko) == true
-                & double.TryParse(KVS.Text, out this.kvs) == true & double.TryParse(KM.Text, out this.km) == true & double.TryParse(KS.Text, out this.ks) == true)
+                & double.TryParse(KVS.Text, out this.kvs) == true & double.TryParse(KM.Text, out this.km) == true & double.TryParse(KS.Text, out this.ks) == true & result.Text != "Результат")
             {
-                this.rslt = this.bt * this.kt * this.kbm * this.ko * this.kvs * this.km * this.ks;
-                result.Text = Convert.ToString(this.rslt);
-                this.tosave = $"Базовый тариф(БТ): {this.bt}" +
+                GetResult(this.bt, this.kt, this.kbm, this.ko, this.kvs, this.km, this.ks, result);
+                this.tosave = $"Базовый тариф (БТ): {this.bt}" +
                 $"\nТерриториальный коэффициент (КТ): {this.kt}" +
                 $"\nКоэффициент \"бонус-малус\" (КБМ): {this.kbm}" +
                 $"\nКоэффициент ограничения (КО): {this.ko}" +
@@ -67,7 +71,7 @@ namespace OSAGOCalc
                 $"\nРезультат: {this.rslt}";
                 Saver saver = new Saver(this.tosave);
             }
-            else MessageBox.Show("Пожалуйста, введите или выберите числовые значения!", "Неверный ввод", MessageBoxButtons.OK);
+            else MessageBox.Show("Пожалуйста, сперва введите или выберите числовые значения коэффициентов и нажмите кнопку \"Расчет\"!", "Неверный ввод", MessageBoxButtons.OK);
         }
     }
 }
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OSAGOCalc;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculationUnitTest()
+        {
+            OSAGOCalc.OSAGOCalc k = new OSAGOCalc.OSAGOCalc();
+            k.rslt = 0; k.bt = 1.25; k.kt = 0.5; k.kbm = 1.23;
+            k.ko = 0.6; k.kvs = 1; k.km = 0.8; k.ks = 1.25;
+
+        }
+    }
+}
